afffc42cfcbdeff5fb093dc706a70ebb8e788caf
Mirza Rayhan <mirzarayhan22@gmail.com>
2022-09-18 13:08:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LogKeuangan_Dipa
+ 
---
+++ app/src/main/java/org/aplas/logkeu/ListCashFlow.java
@@ -0,0 +1,13 @@
+package org.aplas.logkeu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListCashFlow extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_cashflow);
+    }
+}
